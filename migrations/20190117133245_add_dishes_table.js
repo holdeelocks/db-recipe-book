4308acebed423ada04aeef4c1aa7e090a13521d4
Holden B <holden.bucher.92@gmail.com>
2019-01-17 13:37:23 -0600

    first migration of dishes table

---
+++ migrations/20190117133245_add_dishes_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("dishes", function(tbl) {
+    tbl.increments();
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique("uq_dish_name");
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("dishes");
+};
